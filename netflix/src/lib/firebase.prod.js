50d53e6656439510b6f0bc63c3ebe8e405ff0b6e
JORDeN9 <nikhileshwwf@gmail.com>
2024-06-11 11:49:59 +0530

    firebase

--- netflix/src/lib/firebase.prod.js
+++ netflix/src/lib/firebase.prod.js
@@ -1,7 +1,26 @@
+// import firebase from 'firebase/compat/app';
+// // Required for side-effects
+// import 'firebase/firestore';
+// import 'firebase/auth';
+// import { seedDatabase } from '../seed';
+
+// const config = {
+//   apiKey: 'AIzaSyC8WnNWp_dgKeZ9ogOKglBItvTOyIiW4jw',
+//   authDomain: 'netflix-14eaa.firebaseapp.com',
+//   projectId: 'netflix-14eaa',
+//   storageBucket: 'netflix-14eaa.appspot.com',
+//   messagingSenderId: '863228942115',
+//   appId: '1:863228942115:web:a1998ba5c5656e68545e96',
+// };
+
+// const firebaseapp = firebase.initializeApp(config);
+// seedDatabase(firebase);
+
+// export { firebaseapp };
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { seedDatabase } from '../seed';
+// import { seedDatabase } from '../seed';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC8WnNWp_dgKeZ9ogOKglBItvTOyIiW4jw',
@@ -11,12 +30,9 @@ const firebaseConfig = {
   messagingSenderId: '863228942115',
   appId: '1:863228942115:web:a1998ba5c5656e68545e96',
 };
-
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const firestore = getFirestore(firebaseApp);
-const auth = getAuth(firebaseApp);
+const firebaseapp = initializeApp(firebaseConfig);
 
-seedDatabase(firestore);
+//const auth = getAuth(firestore);
 
-export { firebaseApp, firestore, auth };
+export { firebaseapp };
