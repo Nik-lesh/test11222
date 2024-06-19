0fdcd66e5d295559b8715f94ad1566c3856c3fa9
JORDeN9 <nikhileshwwf@gmail.com>
2024-06-19 19:58:02 +0530

    done

--- instagram/src/pages/signup.tsx
+++ instagram/src/pages/signup.tsx
@@ -7,9 +7,10 @@ import {
   UserCredential,
   updateProfile,
 } from "firebase/auth";
-import { firestore, auth } from "../lib/firebase";
+import { firestore } from "../lib/firebase";
 import * as ROUTES from "../constants/routes";
 import { doesUsernameExist } from "../servies/firebase";
+import "./styles/auth.css"; // Import the CSS file
 
 interface SignUpProps {
   navigate: NavigateFunction; // Define the navigate prop
@@ -18,7 +19,6 @@ interface SignUpProps {
 const SignUp: React.FC<SignUpProps> = ({ navigate }) => {
   const context = useContext(FirebaseContext);
   // Type assertion
-
   const { firebase } = context as FirebaseContextProps;
   const auth = getAuth(firebase);
 
@@ -71,69 +71,53 @@ const SignUp: React.FC<SignUpProps> = ({ navigate }) => {
   }, []);
 
   return (
-    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
-        <img
-          src="/images/iphone-with-profile.jpg"
-          alt="iPhone with Instagram app"
-        />
-      </div>
-      <div className="flex flex-col w-2/5">
-        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
-          <h1 className="flex justify-center w-full">
-            <img
-              src="/images/logo.png"
-              alt="Instagram"
-              className="mt-2 w-6/12 mb-4"
-            />
-          </h1>
+    <div className="container">
+      <img
+        src="/images/iphone-with-profile.jpg"
+        alt="iPhone with Instagram app"
+        className="phone-image" // Apply the CSS class here
+      />
+      <div className="form-container">
+        <h1 className="flex justify-center w-full">
+          <img src="/images/logo.png" alt="Instagram" className="mt-2 mb-4" />
+        </h1>
 
-          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-          <form onSubmit={handleSignUp} method="POST">
-            <input
-              aria-label="Enter your username"
-              type="text"
-              placeholder="Username"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setUsername(target.value)}
-              value={username}
-            />
-            <input
-              aria-label="Enter your full name"
-              type="text"
-              placeholder="Full name"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setFullName(target.value)}
-              value={fullName}
-            />
-            <input
-              aria-label="Enter your email address"
-              type="text"
-              placeholder="Email address"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setEmailAddress(target.value)}
-              value={emailAddress}
-            />
-            <input
-              aria-label="Enter your password"
-              type="password"
-              placeholder="Password"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setPassword(target.value)}
-              value={password}
-            />
-            <button
-              disabled={isInvalid}
-              type="submit"
-              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
-            ${isInvalid && "opacity-50"}`}
-            >
-              Sign Up
-            </button>
-          </form>
-        </div>
-        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+        <form onSubmit={handleSignUp} method="POST">
+          <input
+            aria-label="Enter your username"
+            type="text"
+            placeholder="Username"
+            onChange={({ target }) => setUsername(target.value)}
+            value={username}
+          />
+          <input
+            aria-label="Enter your full name"
+            type="text"
+            placeholder="Full name"
+            onChange={({ target }) => setFullName(target.value)}
+            value={fullName}
+          />
+          <input
+            aria-label="Enter your email address"
+            type="text"
+            placeholder="Email address"
+            onChange={({ target }) => setEmailAddress(target.value)}
+            value={emailAddress}
+          />
+          <input
+            aria-label="Enter your password"
+            type="password"
+            placeholder="Password"
+            onChange={({ target }) => setPassword(target.value)}
+            value={password}
+          />
+          <button disabled={isInvalid} type="submit">
+            Sign Up
+          </button>
+        </form>
+        <div className="signup-link-container">
           <p className="text-sm">
             Have an account?{` `}
             <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
