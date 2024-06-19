33c87b241cc679c296265458c61a508a2c12f5e2
JORDeN9 <nikhileshwwf@gmail.com>
2024-06-19 14:09:46 +0530

    types

---
+++ instagram/src/types.ts
@@ -0,0 +1,24 @@
+export interface User {
+  userId: string;
+  username: string;
+  fullName: string;
+  emailAddress: string;
+  following: string[];
+  followers: string[];
+  dateCreated: number;
+}
+
+export interface Photo {
+  photoId: number;
+  userId: string;
+  imageSrc: string;
+  caption: string;
+  likes: string[];
+  comments: { displayName: string; comment: string }[];
+  userLatitude: string;
+  userLongitude: string;
+  dateCreated: number;
+}
+
+export type UserProps = User;
+export type PhotoProps = Photo;
